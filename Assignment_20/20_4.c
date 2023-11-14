20161b95b962b0f59fc19bdaf688cd3517bbae39
Borse-Shubham <shubhamborse0911@gmail.com>
2023-11-14 09:34:30 +0530

    Pattern Priting

---
+++ Assignment_20/20_4.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+void pattern(int iRow, int iCol)
+{
+    int i = 0, j = 0, iCount = 2;
+    char ch = 'a';
+    for(i = 1; i <= iCol; i++)
+    {
+        for(j = 1; j <= iRow; j++)
+        {
+            if(i % 2 == 0)
+            {
+                printf(" %d ",-j);
+            }
+            else
+            {
+                printf("  %d ",j);
+            }
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of Columns and Rows : \n");
+    scanf("%d %d",&iValue1, &iValue2);
+
+    pattern(iValue1, iValue2); 
+
+    return 0;
+}
