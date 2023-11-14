e5598bb24345baea65c2a17d452ea718ca2e3684
Borse-Shubham <shubhamborse0911@gmail.com>
2023-11-14 09:36:21 +0530

    Pattern Printing

---
+++ Assignment_19/19_1.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+void pattern(int iCol, int iRow)
+{
+    int i = 0, j = 0;
+    char ch = 'A';
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = 0; j <= iCol; j++)
+        {
+            printf(" %c ", ch + j);
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of Columns and Rows : \n");
+    scanf("%d %d",&iValue1, &iValue2);
+
+    pattern(iValue1, iValue2); 
+
+    return 0;
+}
