de396e9ef3ece845a9f7e9a788d8b69ba60bee85
Borse-Shubham <shubhamborse0911@gmail.com>
2023-11-15 14:12:55 +0530

    Pattern Printing in C++

---
+++ Assignment_22/22_1.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+
+class Printing
+{
+    public:
+        int iRow,iCol;
+
+    public:
+        Printing(int a, int b)
+        {
+            iRow = a;
+            iCol = b;
+        }
+        void Pattern()
+        {
+            int i = 0, j = 0;
+
+            for(i = 1; i <= iRow; i++)
+            {
+                for(j = 1; j <= iCol; j++)
+                {
+                    if(i >= j )
+                    {
+                        cout<<" * ";
+                    }
+                }
+                cout<<endl;
+            } 
+        }
+};
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    cout<<"Enter number of rows : "<<endl;
+    cin>>iValue1;
+    cout<<"Enter number of Columns : "<<endl;
+    cin>>iValue2;
+
+    Printing obj(iValue1, iValue2);
+
+    obj.Pattern();
+
+    return 0;
+}
