d318935b1a1fe0a0f5193a000a1b266d43e0ce18
Borse-Shubham <shubhamborse0911@gmail.com>
2023-11-16 10:07:22 +0530

    Pattern in C++

---
+++ Assignment_23/23_2.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+
+class Printing
+{
+    public:
+        int iRow,iCol;
+    public:
+        Printing(int X, int Y)
+        {
+            iRow = X;
+            iCol = Y;
+        }
+        void Pattern()
+        {
+            for(int i = iRow; i >= 0; i--)
+            {
+                for(int j = 0; j <= iCol; j++)
+                {
+                    if(i == j)
+                    {
+                        cout<<" # ";
+                    }
+                    else if(i > j)
+                    {
+                        cout<<" * ";
+                    }
+                    else
+                    {
+                        cout<<" @ ";
+                    }
+                }
+                cout<<endl;
+            }
+        }
+};
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    cout<<"Enter number of Rows : ";
+    cin>>iValue1;
+    cout<<"Enter number of Columns : ";
+    cin>>iValue2;
+
+    Printing obj(iValue1, iValue2);
+
+    obj.Pattern();
+
+    return 0;
+}
